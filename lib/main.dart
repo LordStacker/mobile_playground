9a86cebf963ce225cf135029496d872f5ef4f906
Lordstacker <nicoenrique_1994@hotmail.com>
2024-02-08 10:32:32 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const title = 'My Awesome app';
-    /*
+
     final colors = [
       Colors.lightBlue,
       Colors.red,
-      Colors.lightBlue,
-      Colors.red,
-      Colors.lightBlue,
-      Colors.red,
-      Colors.lightBlue,
-      Colors.red,
-      Colors.lightBlue,
-      Colors.red
-    ];*/
+      Colors.deepOrange,
+      Colors.amber,
+      Colors.grey,
+      Colors.purple,
+      Colors.teal,
+      Colors.green,
+      Colors.pink,
+      Colors.white60
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
@@ -33,36 +33,36 @@ class MyApp extends StatelessWidget {
         body: GridView.count(
           crossAxisCount: 2,
           children: List.generate(10, (index) {
-            //final colorIndex = index % colors.length;
+            final colorIndex = index % colors.length;
 
             return Container(
                 width: 80,
                 height: 80,
-                margin: const EdgeInsets.all(5.0),
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
+                margin: const EdgeInsets.all(3.0),
+                decoration:  BoxDecoration(
+                  color: colors[colorIndex],
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                 ));
           }),
         ),
-        floatingActionButton:  FloatingActionButton(
-          onPressed: () { },
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: const Icon(Icons.add),
         ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ) /*,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            )/* ,
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
             label: 'Business',
@@ -72,7 +72,7 @@ class MyApp extends StatelessWidget {
             label: 'School',
           ),*/
           ],
-      ),
+        ),
       ),
     );
   }
